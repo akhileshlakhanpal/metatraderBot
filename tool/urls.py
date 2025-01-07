e081a059b1720c2b88c5e05897973672a3c87206
Akhilesh Lakhanpal <akhileshlakhanpal660@gmail.com>
2025-01-08 03:20:37 +0530

    UI Changes

--- tool/urls.py
+++ tool/urls.py
@@ -1,32 +1,12 @@
-
-
-from django.urls import path
-from django_distill import distill_path
 from . import views
+from django.urls import path
 
-def get_signin_url():
-    return None
-
-def get_dashboard_url():
-    return None
-
-def get_manage_slave_url():
-    return None
-
-def get_manage_master_url():
-    return None
-
-def get_groups_copier_url():
-    return None
-
-def get_servers_url():
-    return None
 
 urlpatterns = [
-    distill_path('', views.signin, name='signin', distill_func=get_signin_url),
-    distill_path('dashboard', views.dashboard, name='dashboard', distill_func=get_dashboard_url),
-    distill_path('manage_slave', views.manage_slave, name='manage_slave', distill_func=get_manage_slave_url),
-    distill_path('manage_master', views.manage_master, name='manage_master', distill_func=get_manage_master_url),
-    distill_path('groups_copier', views.groups_copier, name='groups_copier', distill_func=get_groups_copier_url),
-    distill_path('servers', views.servers, name='servers', distill_func=get_servers_url),
-]
+    path('', views.signin,name='signin'),
+    path('dashboard', views.dashboard,name='dashboard'),
+    path('manage_slave', views.manage_slave,name='manage_slave'),
+    path('manage_master', views.manage_master,name='manage_master'),
+    path('groups_copier', views.groups_copier,name='groups_copier'),
+    path('servers', views.servers,name='servers'),
+    ]
